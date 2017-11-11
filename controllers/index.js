cfcb10635d34321c25ad7d8527ea8954704114a4
Santiago Buenahora <santib97@gmail.com>
2017-11-11 17:13:32 -0500

    server routing

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,20 @@
-var routes = require('./routes.js');
-var tweets = require('./tweet.js');
 
-routes.app.listen(3000);
-console.log("Listening on localhost:3000");
+// models
+var mongoose = require('mongoose');
+// db configs
+var db_username = process.env.DB_USERNAME.trim();
+var db_password = process.env.DB_PASSWORD.trim();
+var db_url = ['mongodb://', db_username, ':', db_password, '@ds155695.mlab.com:55695/chessbird'].join('');
+mongoose.connect(db_url, { useMongoClient: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// schema
+require('../models/games.js');
+
+// establish routing
+var routes = require('./routes.js');
+routes.app.listen(process.env.PORT || 3000, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, routes.app.settings.env);
+});
+
+var tweets = require('./tweet.js');
--- controllers/routes.js
+++ controllers/routes.js
@@ -2,14 +2,31 @@ var express = require('express')
 var app = express()
 var exphbs  = require('express-handlebars');
 
-// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
+var mongoose = require('mongoose'),
+  Game = mongoose.model('Game');
 
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 app.get('/', function (req, res) {
   res.render('index');
-})
+});
+
+app.post('/api/new_game', function (req, res) {
+	console.log(req.query);
+});
+
+app.get('/api/state/:game_id', function (req, res) {
+  Game.find({}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+});
+
+// app.post('/api/new_move/<game_id>', function (req, res) {
+// 	// get current 
+
+// });
 
 exports.app = app
