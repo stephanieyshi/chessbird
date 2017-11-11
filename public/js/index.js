b056be517a6b93d4c233600dfc39e3bdb3b5a129
esqu1 <brandbest1@gmail.com>
2017-11-11 13:33:11 -0500

    refactored frontend js code

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,9 @@
-var board = ChessBoard('board', 'start');
+$(document).ready(function() {
+	var board = ChessBoard('board', {
+		draggable: true,
+		dropOffBoard: 'trash',
+		sparePieces: true
+	});
+	$('#startBtn').on('click', board.start);
+	$('#clearBtn').on('click', board.clear);
+});
