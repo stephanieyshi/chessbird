c5d53b29eadef89f391449c59cb60983f2fecc24
esqu1 <brandbest1@gmail.com>
2017-11-11 13:15:30 -0500

    api routes

--- controllers/routes.js
+++ controllers/routes.js
@@ -4,13 +4,28 @@ var app = express()
 app.set('json spaces', 4)
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  	res.send('Hello World');
 })
 
 app.get('/api', (req, res) => {
-  res.send(JSON.stringify({
-  	test: 'yeah'
-  }))
+  	res.json({
+  		test: 'yeah'
+  	})
 })
+
+app.get('/api/valid', (req, res) => {
+	// Takes in the board state as a string and a move and returns if it is valid
+})
+
+app.get('/api/check', (req, res) => {
+	// Takes in a board and checks if a player is in check; returns which
+	// player is in check right now
+})
+
+app.get('/api/checkmate', (req, res) => {
+	// Checks if a player has lost
+})
+
+
 
 exports.app = app
