553068a432faf6927c594b165c95311e30a7902b
LUCIAHUO <luciahuo@sas.upenn.edu>
2017-11-11 14:12:57 -0500

    modifications to tweet.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,19 @@
+import {convertChessToString} from 'tweet'
 var routes = require('./routes.js');
 var tweets = require('./tweet.js');
 
 routes.app.listen(3000);
 console.log("Listening on localhost:3000");
+
+var error = function (err, response, body) {
+   console.log('ERROR [%s]', err);
+ };
+
+var success = function (data) {
+  console.log('Data [%s]', data);
+};
+
+function main () {
+  // test 
+  tweets.postTweet({status:"Hello World"}, error, success);
+}
