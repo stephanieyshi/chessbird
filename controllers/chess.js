74a1b44fff44d022d1f483e959f1a7faec39e46c
esqu1 <brandbest1@gmail.com>
2017-11-11 12:49:36 -0500

    route for the chess logic api

--- controllers/chess.js
+++ controllers/chess.js
@@ -1,18 +1,36 @@
 /**
  * move = [first, second]
- * pawn is 0
- * king is 1
- * queen is 2
- * rook is 3
- * knight is 4
- * bishop is 5
+ * empty is 0
+ * pawn is 1
+ * king is 2
+ * queen is 3
+ * rook is 4
+ * knight is 5
+ * bishop is 6
  */
+
+function neighbors(x, y) {
+	var arr = [];
+		for (i = x - 1; i <= x + 1; i++) {
+			for (j = y - 1; j <= y + 1; j++) {
+				if (i >= 0 && i < 8 && j >= 0 && j < 8) {
+					arr.push([i,j]);
+				}
+			}
+		}
+	return arr;
+}
+
+function isCheck(board) {
+	
+}
 
 function King(x, y, board) {
 	this.x = x;
 	this.y = y;
-	this.board;
+	this.board = board;
 	this.validMove = function (x, y) {
+		// need to check if king is checkmate
 		for (l in this.validSquares()) {
 			if (l[0] == x && l[1] == y) {
 				return true;
@@ -21,7 +39,6 @@ function King(x, y, board) {
 		return false;
 	}
 	this.validSquares = function () {
-		var arr = [];
 		for (i = x - 1; i <= x + 1; i++) {
 			for (j = y - 1; j <= y + 1; j++) {
 				if (i >= 0 && i < 8 && j >= 0 && j < 8) {
@@ -33,9 +50,10 @@ function King(x, y, board) {
 	}
 }
 
-function Queen(x, y) {
+function Queen(x, y, board) {
 	this.x = x;
 	this.y = y;
+	this.board = board;
 	this.validMove = function (x, y) {
 		for (l in this.validSquares()) {
 			if (l[0] == x && l[1] == y) {
--- controllers/routes.js
+++ controllers/routes.js
@@ -1,8 +1,16 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
+app.set('json spaces', 4)
+
+app.get('/', (req, res) => {
   res.send('Hello World');
+})
+
+app.get('/api', (req, res) => {
+  res.send(JSON.stringify({
+  	test: 'yeah'
+  }))
 })
 
 exports.app = app
