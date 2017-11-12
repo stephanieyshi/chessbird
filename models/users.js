7d92e66534bc0545ecf62bf225f0d29fedac3e3e
Santiago Buenahora <santib97@gmail.com>
2017-11-12 00:26:07 -0500

    Add user schema

---
+++ models/users.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//import dependency
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//create new instance of the mongoose.schema. the schema takes an 
+//object that shows the shape of your database entries.
+var UsersSchema = new Schema({
+ 	screen_name: String,
+ 	access_token: String,
+ 	access_secret: String
+});
+
+//export our module to use in server.js
+module.exports = mongoose.model('User', UsersSchema);
